d3719733daf3f40f5a400dc57810bed856b61025
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-05 17:22:34 -0400

    Basic html/ng

---
+++ public/index.js
@@ -0,0 +1,10 @@
+app.controller("mainController", ['$scope','$http','$sce', function($scope, $http, $sce) {
+  $scope.view = 0;
+  $scope.currid = "home";
+
+  $scope.changeActive = function(id) {
+    document.getElementById($scope.currid).className = 'nav-link'; 
+    document.getElementById(id).className = 'nav-link active';
+    $scope.currid = id;
+}
+}]);
